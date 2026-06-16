4287eae3dcf50896ad11a11117a2a7b39a87f81c
Satria Irawan <satriai418@gmail.com>
2026-06-16 19:55:26 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -36,4 +36,69 @@ class Controller extends BaseController
 
         return $this->access;
     }
+
+    /**
+     * Get sidebar access list for a given user group.
+     *
+     * This method retrieves all pages that have 'Read' access for a specific group.
+     * The result is used to build the sidebar menu.
+     *
+     * @param int $groupId
+     * @return \Illuminate\Support\Collection
+     */
+    public static function getSidebarAccess(int $groupId): \Illuminate\Support\Collection
+    {
+        return \Illuminate\Support\Facades\DB::table('group_pages')
+            ->join('pages', 'pages.id', '=', 'group_pages.page_id')
+            ->where('group_pages.group_id', $groupId)
+            ->where('group_pages.access', 1)
+            ->where('pages.action', 'Read')
+            ->select('pages.module', 'pages.page_name', 'pages.action')
+            ->get();
+    }
+
+    /**
+ * Get modules that have more than one page with 'Read' access, along with a sample page.
+ *
+ * @param int $groupId
+ * @return \Illuminate\Support\Collection
+ */
+public static function getHomeAccessModules(int $groupId): \Illuminate\Support\Collection
+{
+    $modules = \Illuminate\Support\Facades\DB::table('group_pages')
+        ->join('pages', 'pages.id', '=', 'group_pages.page_id')
+        ->where('group_pages.group_id', $groupId)
+        ->where('pages.action', 'Read')
+        ->where('group_pages.access', 1)
+        ->select('pages.module', DB::raw('COUNT(DISTINCT pages.page_name) as total'))
+        ->groupBy('pages.module')
+        ->having('total', '>', 1)
+        ->pluck('module');
+
+    if ($modules->isEmpty()) {
+        return collect();
+    }
+
+    $result = collect();
+    foreach ($modules as $module) {
+        $page = \Illuminate\Support\Facades\DB::table('group_pages')
+            ->join('pages', 'pages.id', '=', 'group_pages.page_id')
+            ->where('group_pages.group_id', $groupId)
+            ->where('pages.module', $module)
+            ->where('pages.action', 'Read')
+            ->where('group_pages.access', 1)
+            ->select('pages.page_name', 'pages.route')
+            ->first();
+
+        if ($page) {
+            $result->push((object) [
+                'module' => $module,
+                'page_name' => $page->page_name,
+                'route' => $page->route,
+            ]);
+        }
+    }
+
+    return $result;
+}
 }
